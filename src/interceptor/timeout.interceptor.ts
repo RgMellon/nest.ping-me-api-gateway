3a71bf2587a72185b8932927da298975cb63971e
RgMellon <renan_gian@hotmail.com>
2024-11-24 14:25:46 -0300

    feat: add timeout

---
+++ src/interceptor/timeout.interceptor.ts
@@ -0,0 +1,20 @@
+import {
+  CallHandler,
+  ExecutionContext,
+  Injectable,
+  NestInterceptor,
+} from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { timeout } from 'rxjs/operators';
+
+// Utilizado para interceptar uma chamada e fazer algo
+
+@Injectable()
+export class TimeoutInterceptor implements NestInterceptor {
+  intercept(
+    context: ExecutionContext,
+    next: CallHandler<any>,
+  ): Observable<any> | Promise<Observable<any>> {
+    return next.handle().pipe(timeout(10000));
+  }
+}
